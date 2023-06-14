7b88190a936259e8d92e34d3b1f72358024bb38a
Yeongjin Gwak <yjgwak.cs@gaml.com>
2023-06-14 18:23:33 +0900

    feat. add email

--- scripts.js
+++ scripts.js
@@ -24,3 +24,17 @@ document
     document.querySelector("#cv").style.display = "none";
     document.querySelector("#research").style.display = "block";
   });
+
+var user = "yjgwak.cs";
+var domain = "gmail";
+var element = ".com";
+var elementTag =
+  '<a href="mailto:' +
+  user +
+  "@" +
+  domain +
+  element +
+  '"><img src="https://cdn-icons-png.flaticon.com/512/281/281769.png" alt="Email" style="height:20px; width:20px;"></a>';
+document
+  .querySelector(".profile-name")
+  .insertAdjacentHTML("beforeend", elementTag);
